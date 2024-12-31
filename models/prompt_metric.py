f7142d69bbb6b163004d972a02120a42221697e7
wonkycoin <34885707-wonkycoin@users.noreply.replit.com>
2024-12-31 18:35:13 +0000

    Checkpoint after starting plan

--- models/prompt_metric.py
+++ models/prompt_metric.py
@@ -3,7 +3,7 @@ from database import db
 
 class PromptMetric(db.Model):
     __tablename__ = 'prompt_metrics'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     prompt_type = db.Column(db.String(50))
@@ -13,6 +13,6 @@ class PromptMetric(db.Model):
     cache_hit = db.Column(db.Boolean)
     prompt_length = db.Column(db.Integer)
     error_message = db.Column(db.Text)
-    
+
     def __repr__(self):
         return f'<PromptMetric {self.id} {self.prompt_type}>'
